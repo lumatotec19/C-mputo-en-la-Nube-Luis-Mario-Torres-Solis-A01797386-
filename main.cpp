471b39af3a1445d69176e858f9e1cdcbccc4adad
lumatotec19 <a01797386@tec.mx>
2026-02-01 15:54:48 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-/******************************************************************************
+/*
+ * Nombre: Luis Mario Torres Solis
+ * Matrícula: A01797386
+ * Materia: Cómputo en la nube
+ * Actividad: Programación de una solución paralela
+ * * NOTA DE COMPILACIÓN:
+ * Este programa utiliza la librería OpenMP. Para que funcione el paralelismo
+ * correctamente, se debe compilar agregando la bandera -fopenmp.
+ * Ejemplo en terminal: g++ main.cpp -o tarea -fopenmp
+ */
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
